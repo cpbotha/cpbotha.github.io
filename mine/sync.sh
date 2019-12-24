a322c0ff76a1d7067008333dbc04eed0afd88e85
Charl P. Botha (X1E) <cpbotha@vxlabs.com>
2019-12-24 17:13:07 +0200

    Add comment re rsync issues

--- mine/sync.sh
+++ mine/sync.sh
@@ -1,5 +1,7 @@
-#!/bin/bash
 hugo -d ~/Downloads/cpbotha.net
+
+echo "HUGO DONE"
+
 # sync contents of public dir into static_cpbothanet/
 # don't add --delete, you always lose stuff you did not intend to lose! (last time .htaccess)
 #
@@ -9,3 +11,10 @@ rsync --info=progress2,stats1 -a --exclude=.htaccess --exclude=pcdc --exclude=fi
 # funny thing: on windows, both wsl and powershell, the scroll buffer gets
 # full, and then the rsync will stop, and then you have to resize the window
 # multiple times to finish the transfer. The info config helps with this!
+
+# this also happens when running /bin/bash from Emacs term.el
+# and it even happens when I redirect output to a file.
+# prepending stdbuf -o0 also does not help
+
+# FRUSTRATING. I can run the rsync command by itself. progress output looks
+# different (percentage not updating), but it runs to completion.
